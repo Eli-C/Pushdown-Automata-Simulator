e18da35c1fcc3f0bdc061cc9517deb2fb2ec5804
Eun Kyu Choi <choi2nkyu@gmail.com>
2018-08-27 14:59:48 -0400

    Kyu: integrated classes Pila and AutomataState into Automata and Rules. Added class Simulate to solve algorithms.

--- src/application/Automata.java
+++ src/application/Automata.java
@@ -1,29 +1,49 @@
 package application;
 
+import java.util.ArrayList;
+
 public class Automata {
 	
-	private AutomataState[] Q;
+	private String[] Q;
 	private char[] X;
 	private char[] P;
-	private AutomataState q0;
+	private String q0;
 	private String Z0;
-	private AutomataState[] F;
+	private String[] F;
+	private ArrayList<Rules> rulesArray;
 	
-	public Automata(AutomataState[] Q, char[] X, char[] P, AutomataState q0, String Z0, AutomataState[] F)
+	public Automata(String[] Q, char[] X, char[] P, String q0, String Z0, String[] F, ArrayList<Rules> rules)
 	{
 		this.Q = Q;
 		this.X = X;
 		this.P = P;
-		this.q0 = q0;
+		this.q0 = q0; 
 		this.Z0 = Z0;
-		this.F = F;		
+		this.F = F;
+		this.rulesArray = rules;
 	}
 	
-	public AutomataState[] getQ() 
+	public Automata() {
+		
+	}
+	
+	public void setRules(Rules rule) {
+		rulesArray.add(rule);
+	}
+	
+	public ArrayList<Rules> getRules() {
+		return rulesArray;
+	}
+	
+	public Rules getRule(int i) {
+		return rulesArray.get(i);
+	}
+	
+	public String[] getQ() 
 	{
 		return Q;
 	}
-	public void setQ(AutomataState[] q) 
+	public void setQ(String[] q) 
 	{
 		Q = q;
 	}
@@ -43,11 +63,11 @@ public class Automata {
 	{
 		P = p;
 	}
-	public AutomataState getQ0() 
+	public String getQ0() 
 	{
 		return q0;
 	}
-	public void setQ0(AutomataState q0) 
+	public void setQ0(String q0) 
 	{
 		this.q0 = q0;
 	}
@@ -59,11 +79,11 @@ public class Automata {
 	{
 		Z0 = z0;
 	}
-	public AutomataState[] getF() 
+	public String[] getF() 
 	{
 		return F;
 	}
-	public void setF(AutomataState[] f) 
+	public void setF(String[] f) 
 	{
 		F = f;
 	}
--- src/application/Rules.java
+++ src/application/Rules.java
@@ -3,14 +3,14 @@ package application;
 public class Rules {
 	
 	
-	private AutomataState actualState;
+	private String actualState;
 	private char input; 
-	private Pila pilaActual;
-	private AutomataState futureState;
-	private Pila pilaFutura;
+	private String pilaActual;
+	private String futureState;
+	private String pilaFutura;
 	
 	
-	public Rules (AutomataState actualState, char input, Pila pilaActual, AutomataState futureState, Pila pilaFutura)
+	public Rules (String actualState, char input, String pilaActual, String futureState, String pilaFutura)
 	{
 		this.actualState = actualState;
 		this.input = input;
@@ -19,11 +19,11 @@ public class Rules {
 		this.pilaFutura = pilaFutura;
 	}
 	
-	public AutomataState getActualState() 
+	public String getActualState() 
 	{
 		return actualState;
 	}
-	public void setActualState(AutomataState actualState) 
+	public void setActualState(String actualState) 
 	{
 		this.actualState = actualState;
 	}
@@ -35,27 +35,27 @@ public class Rules {
 	{
 		this.input = input;
 	}
-	public Pila getPilaActual() 
+	public String getPilaActual() 
 	{
 		return pilaActual;
 	}
-	public void setPilaActual(Pila pilaActual) 
+	public void setPilaActual(String pilaActual) 
 	{
 		this.pilaActual = pilaActual;
 	}
-	public AutomataState getFutureState() 
+	public String getFutureState() 
 	{
 		return futureState;
 	}
-	public void setFutureState(AutomataState futureState) 
+	public void setFutureState(String futureState) 
 	{
 		this.futureState = futureState;
 	}
-	public Pila getPilaFutura() 
+	public String getPilaFutura() 
 	{
 		return pilaFutura;
 	}
-	public void setPilaFutura(Pila pilaFutura) 
+	public void setPilaFutura(String pilaFutura) 
 	{
 		this.pilaFutura = pilaFutura;
 	}
