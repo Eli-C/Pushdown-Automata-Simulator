abdac2ae9d1f44776f6f7e3f10bfc9c4afba0279
Eun Kyu Choi <choi2nkyu@gmail.com>
2018-08-28 18:22:29 -0400

    Kyu: finished algorithm

--- src/application/Simulate.java
+++ src/application/Simulate.java
@@ -1,39 +1,43 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Simulate {
-
-	private String input;
-	private String initialState;
-	public Stack<String> stack;
-	private String result;
+	private boolean result;
+	private ArrayList<Rules> rules;
 	
 	public Simulate() {
-		this.stack.push("Z");
-	}
-	
-	public String testWord(String input, String initialState) {
-		this.input = input;
-		this.initialState = initialState;
-		Stack<String> stackcopy = stack;
-		
-		for (int i=0; i<input.length(); i++) {
-			String read = String.valueOf(input.charAt(i));
-			
-		}
-		
-		return result;
-	}
-	
-	public int findBestRule(char word, String state) {
 		Automata automata = new Automata();
 		ArrayList<Rules> rules = automata.getRules();
-		Stream<Rules> possibleRules = rules.stream().filter(rule -> ((rule.getInput() == word) && (rule.getActualState() == state)));
-		
-		return 0;
+		this.rules = rules;
 	}
 	
+	public boolean testWord(String input, String state, Stack<Character> stack) {
+		
+		if(input.isEmpty() && state == "Aceptar" || stack.isEmpty()) {
+			return true;
+		}
+		char pilaActual = stack.pop();
+		char word = Character.valueOf(input.charAt(0));
+		Stream<Rules> possibleRulesStream = rules.stream().filter(rule -> ((rule.getInput() == word) && (rule.getActualState() == state) && (rule.getPilaActual() == pilaActual)));
+		List<Rules> possibleRules = possibleRulesStream.collect(Collectors.toList());
+
+		for(int i=0; i<possibleRules.size(); i++) {
+			if(possibleRules.get(i).getPilaFutura() == 'l') {
+				stack.pop();
+			} else {
+				char addStack = possibleRules.get(i).getPilaFutura();
+				stack.push(addStack);
+			}
+			
+			if(testWord(input.substring(1), possibleRules.get(i).getFutureState(), stack)){
+				return true;
+			}
+		}
+		return false;
+	}
 }
